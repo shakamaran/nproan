9e66aee98926b9bc77db22bec7581c8339f5174e
Florian Heinrich <florianheinrich@gmx.at>
2024-04-23 22:41:23 +0200

    add a file handler to the logging class

--- logger.py
+++ logger.py
@@ -1,10 +1,14 @@
 import logging
 
 class Logger:
-    def __init__(self, logger_name, level=logging.INFO):
+    def __init__(self, logger_name, level=logging.INFO, file_name='logfile.log'):
         # Create a logger
         self.logger = logging.getLogger(logger_name)
         self.logger.setLevel(level)
+
+        # Create a file handler
+        fh = logging.FileHandler(file_name)
+        fh.setLevel(level)
 
         # Create a console handler
         ch = logging.StreamHandler()
@@ -13,10 +17,12 @@ class Logger:
         # Create a formatter
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        # Add the formatter to the console handler
+        # Add the formatter to the handlers
+        fh.setFormatter(formatter)
         ch.setFormatter(formatter)
 
-        # Add the console handler to the logger
+        # Add the handlers to the logger
+        self.logger.addHandler(fh)
         self.logger.addHandler(ch)
 
     def get_logger(self):
